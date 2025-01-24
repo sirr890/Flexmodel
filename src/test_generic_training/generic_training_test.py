b991a0fd9d9f35d140e6e5d30d844936957b75e6
sirr890 <sirr890@gmail.com>
2025-01-24 15:26:14 -0500

    modificacion

--- src/test_generic_training/generic_training_test.py
+++ src/test_generic_training/generic_training_test.py
@@ -6,7 +6,7 @@ import os
 import sys
 import unittest
 
-# Agregar el directorio raíz del proyecto dinámicamente al sys.path
+# AAgregar el directorio raíz del proyecto dinámicamente al sys.path
 project_root = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../"))
 if project_root not in sys.path:
     sys.path.insert(0, project_root)
